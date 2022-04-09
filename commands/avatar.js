6ffd27fc125a049905686af38f4eb8983fd31a5c
Loïs Libessart <lois.libessart@gmail.com>
2022-04-09 03:13:17 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'avatar',
   aliases: ['icon', 'pfp', 'pp'],
-  description: 'Returns your avatar or the user you mentionned\'s one.',
+  description: 'Retourne votre avatar ou celui de la personne que vous avez mentionée.',
   
   async execute(bot, message, args) {
     let member = message.mentions.users.first();
@@ -11,7 +11,7 @@ module.exports = {
     let avatar = member.avatarURL({size: 1024});
     
     let embed = new Discord.MessageEmbed()
-      .setTitle(`${member.username}'s Avatar:`)
+      .setTitle(`Avatar de ${member.username} :`)
       .setImage(avatar)
       .setFooter('avatar')
       .setTimestamp()
